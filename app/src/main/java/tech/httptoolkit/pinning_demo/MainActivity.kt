80c728336834098a91dd99b391557c17295b9b59
Tim Perry <pimterry@gmail.com>
2021-06-29 11:53:51 +0200

    Set up a (non-functional) inicial app and UI

---
+++ app/src/main/java/tech/httptoolkit/pinning_demo/MainActivity.kt
@@ -0,0 +1,11 @@
+package tech.httptoolkit.pinning_demo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
